c34d160814485d85170d8819f760989ce6be93de
DanyalMaq <danyalmaqbool@hotmail.com>
2023-11-29 00:03:17 -0600

    rename

--- transpose.sh
+++ transpose.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
-#SBATCH --job-name=test
+#SBATCH --job-name=transpose
 #SBATCH --partition=instruction
 #SBATCH --time=00-00:01:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:2
 #SBATCH --mem=20G
-#SBATCH --output=column.out
-#SBATCH --error=column.err
+#SBATCH --output=transpose.out
+#SBATCH --error=transpose.err
 
 module load nvidia/cuda/11.8.0
 nvcc column.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -lcurand -o  t
 
-./t 128 2
+./t 64 2
