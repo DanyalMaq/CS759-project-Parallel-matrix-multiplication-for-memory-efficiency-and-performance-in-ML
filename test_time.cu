f492c7097ac87d3e3c84a3a4b91768d66c9a5e41
DanyalMaq <danyalmaqbool@hotmail.com>
2023-11-29 01:44:35 -0600

    added main timing file

---
+++ test_time.cu
@@ -0,0 +1,25 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "matmul.cuh"
+#include "utils.cuh"
+using namespace std;
+
+// TODO
+// Convert the managed and async files to kernels to be called
+
+int main(int argc, char** argv)
+{
+    // Basic CPU call and time 
+
+    // Pragma call and time
+
+    // Async kernel call and time
+
+    // Managed memory call and time
+
+    // Basic allocation call and time
+}
